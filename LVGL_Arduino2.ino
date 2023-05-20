a17173b8e95051a5ff3127fb9138b9e9262aac51
Aziz Faozi <faoziaziz@s.itb.ac.id>
2023-05-20 19:14:00 +0700

    code berhasil esp32

---
+++ LVGL_Arduino2.ino
@@ -0,0 +1,371 @@
+#include <lvgl.h>
+ /* TFT instance */
+
+#include <LovyanGFX.hpp>
+
+// ESP32でLovyanGFXを独自設定で利用する場合の設定例
+
+/// 独自の設定を行うクラスを、LGFX_Deviceから派生して作成します。
+class LGFX : public lgfx::LGFX_Device
+{
+/*
+ クラス名は"LGFX"から別の名前に変更しても構いません。
+ AUTODETECTと併用する場合は"LGFX"は使用されているため、LGFX以外の名前に変更してください。
+ また、複数枚のパネルを同時使用する場合もそれぞれに異なる名前を付けてください。
+ ※ クラス名を変更する場合はコンストラクタの名前も併せて同じ名前に変更が必要です。
+
+ 名前の付け方は自由に決めて構いませんが、設定が増えた場合を想定し、
+ 例えばESP32 DevKit-CでSPI接続のILI9341の設定を行った場合、
+  LGFX_DevKitC_SPI_ILI9341
+ のような名前にし、ファイル名とクラス名を一致させておくことで、利用時に迷いにくくなります。
+//*/
+
+
+// 接続するパネルの型にあったインスタンスを用意します。
+//lgfx::Panel_GC9A01      _panel_instance;
+//lgfx::Panel_GDEW0154M09 _panel_instance;
+//lgfx::Panel_HX8357B     _panel_instance;
+//lgfx::Panel_HX8357D     _panel_instance;
+//lgfx::Panel_ILI9163     _panel_instance;
+  lgfx::Panel_ILI9341     _panel_instance;
+//lgfx::Panel_ILI9342     _panel_instance;
+//lgfx::Panel_ILI9481     _panel_instance;
+//lgfx::Panel_ILI9486     _panel_instance;
+//lgfx::Panel_ILI9488     _panel_instance;
+//lgfx::Panel_IT8951      _panel_instance;
+//lgfx::Panel_RA8875      _panel_instance;
+//lgfx::Panel_SH110x      _panel_instance; // SH1106, SH1107
+//lgfx::Panel_SSD1306     _panel_instance;
+//lgfx::Panel_SSD1327     _panel_instance;
+//lgfx::Panel_SSD1331     _panel_instance;
+//lgfx::Panel_SSD1351     _panel_instance; // SSD1351, SSD1357
+//lgfx::Panel_SSD1963     _panel_instance;
+//lgfx::Panel_ST7735      _panel_instance;
+//lgfx::Panel_ST7735S     _panel_instance;
+//lgfx::Panel_ST7789      _panel_instance;
+//lgfx::Panel_ST7796      _panel_instance;
+
+
+// パネルを接続するバスの種類にあったインスタンスを用意します。
+  lgfx::Bus_SPI        _bus_instance;   // SPIバスのインスタンス
+//lgfx::Bus_I2C        _bus_instance;   // I2Cバスのインスタンス
+//lgfx::Bus_Parallel8  _bus_instance;   // 8ビットパラレルバスのインスタンス
+
+// バックライト制御が可能な場合はインスタンスを用意します。(必要なければ削除)
+  lgfx::Light_PWM     _light_instance;
+
+// タッチスクリーンの型にあったインスタンスを用意します。(必要なければ削除)
+  lgfx::Touch_FT5x06           _touch_instance; // FT5206, FT5306, FT5406, FT6206, FT6236, FT6336, FT6436
+//lgfx::Touch_GSL1680E_800x480 _touch_instance; // GSL_1680E, 1688E, 2681B, 2682B
+//lgfx::Touch_GSL1680F_800x480 _touch_instance;
+//lgfx::Touch_GSL1680F_480x272 _touch_instance;
+//lgfx::Touch_GSLx680_320x320  _touch_instance;
+//lgfx::Touch_GT911            _touch_instance;
+//lgfx::Touch_STMPE610         _touch_instance;
+//lgfx::Touch_TT21xxx          _touch_instance; // TT21100
+//lgfx::Touch_XPT2046          _touch_instance;
+
+public:
+
+  // コンストラクタを作成し、ここで各種設定を行います。
+  // クラス名を変更した場合はコンストラクタも同じ名前を指定してください。
+  LGFX(void)
+  {
+    { // バス制御の設定を行います。
+      auto cfg = _bus_instance.config();    // バス設定用の構造体を取得します。
+
+// SPIバスの設定
+      cfg.spi_host = VSPI_HOST;     // 使用するSPIを選択  ESP32-S2,C3 : SPI2_HOST or SPI3_HOST / ESP32 : VSPI_HOST or HSPI_HOST
+      // ※ ESP-IDFバージョンアップに伴い、VSPI_HOST , HSPI_HOSTの記述は非推奨になるため、エラーが出る場合は代わりにSPI2_HOST , SPI3_HOSTを使用してください。
+      cfg.spi_mode = 0;             // SPI通信モードを設定 (0 ~ 3)
+      cfg.freq_write = 40000000;    // 送信時のSPIクロック (最大80MHz, 80MHzを整数で割った値に丸められます)
+      cfg.freq_read  = 16000000;    // 受信時のSPIクロック
+      cfg.spi_3wire  = true;        // 受信をMOSIピンで行う場合はtrueを設定
+      cfg.use_lock   = true;        // トランザクションロックを使用する場合はtrueを設定
+      cfg.dma_channel = SPI_DMA_CH_AUTO; // 使用するDMAチャンネルを設定 (0=DMA不使用 / 1=1ch / 2=ch / SPI_DMA_CH_AUTO=自動設定)
+      // ※ ESP-IDFバージョンアップに伴い、DMAチャンネルはSPI_DMA_CH_AUTO(自動設定)が推奨になりました。1ch,2chの指定は非推奨になります。
+      cfg.pin_sclk = 14;            // SPIのSCLKピン番号を設定
+      cfg.pin_mosi = 13;            // SPIのMOSIピン番号を設定
+      cfg.pin_miso = 19;            // SPIのMISOピン番号を設定 (-1 = disable)
+      cfg.pin_dc   = 2;            // SPIのD/Cピン番号を設定  (-1 = disable)
+
+      _bus_instance.config(cfg);    // 設定値をバスに反映します。
+      _panel_instance.setBus(&_bus_instance);      // バスをパネルにセットします。
+    }
+
+    { // 表示パネル制御の設定を行います。
+      auto cfg = _panel_instance.config();    // 表示パネル設定用の構造体を取得します。
+
+      cfg.pin_cs           =    15;  // CSが接続されているピン番号   (-1 = disable)
+      cfg.pin_rst          =    4;  // RSTが接続されているピン番号  (-1 = disable)
+      cfg.pin_busy         =    -1;  // BUSYが接続されているピン番号 (-1 = disable)
+
+      // ※ 以下の設定値はパネル毎に一般的な初期値が設定されていますので、不明な項目はコメントアウトして試してみてください。
+
+      cfg.panel_width      =   240;  // 実際に表示可能な幅
+      cfg.panel_height     =   320;  // 実際に表示可能な高さ
+      cfg.offset_x         =     0;  // パネルのX方向オフセット量
+      cfg.offset_y         =     0;  // パネルのY方向オフセット量
+      cfg.offset_rotation  =     0;  // 回転方向の値のオフセット 0~7 (4~7は上下反転)
+      cfg.dummy_read_pixel =     8;  // ピクセル読出し前のダミーリードのビット数
+      cfg.dummy_read_bits  =     1;  // ピクセル以外のデータ読出し前のダミーリードのビット数
+      cfg.readable         =  true;  // データ読出しが可能な場合 trueに設定
+      cfg.invert           = false;  // パネルの明暗が反転してしまう場合 trueに設定
+      cfg.rgb_order        = false;  // パネルの赤と青が入れ替わってしまう場合 trueに設定
+      cfg.dlen_16bit       = false;  // 16bitパラレルやSPIでデータ長を16bit単位で送信するパネルの場合 trueに設定
+      cfg.bus_shared       =  true;  // SDカードとバスを共有している場合 trueに設定(drawJpgFile等でバス制御を行います)
+
+// 以下はST7735やILI9163のようにピクセル数が可変のドライバで表示がずれる場合にのみ設定してください。
+//    cfg.memory_width     =   240;  // ドライバICがサポートしている最大の幅
+//    cfg.memory_height    =   320;  // ドライバICがサポートしている最大の高さ
+
+      _panel_instance.config(cfg);
+    }
+
+//*
+    { // バックライト制御の設定を行います。（必要なければ削除）
+      auto cfg = _light_instance.config();    // バックライト設定用の構造体を取得します。
+
+      cfg.pin_bl = 32;              // バックライトが接続されているピン番号
+      cfg.invert = false;           // バックライトの輝度を反転させる場合 true
+      cfg.freq   = 44100;           // バックライトのPWM周波数
+      cfg.pwm_channel = 7;          // 使用するPWMのチャンネル番号
+
+      _light_instance.config(cfg);
+      _panel_instance.setLight(&_light_instance);  // バックライトをパネルにセットします。
+    }
+//*/
+
+//*
+    { // タッチスクリーン制御の設定を行います。（必要なければ削除）
+      auto cfg = _touch_instance.config();
+
+      cfg.x_min      = 0;    // タッチスクリーンから得られる最小のX値(生の値)
+      cfg.x_max      = 239;  // タッチスクリーンから得られる最大のX値(生の値)
+      cfg.y_min      = 0;    // タッチスクリーンから得られる最小のY値(生の値)
+      cfg.y_max      = 319;  // タッチスクリーンから得られる最大のY値(生の値)
+      cfg.pin_int    = 38;   // INTが接続されているピン番号
+      cfg.bus_shared = true; // 画面と共通のバスを使用している場合 trueを設定
+      cfg.offset_rotation = 0;// 表示とタッチの向きのが一致しない場合の調整 0~7の値で設定
+
+// SPI接続の場合
+      cfg.spi_host = VSPI_HOST;// 使用するSPIを選択 (HSPI_HOST or VSPI_HOST)
+      cfg.freq = 1000000;     // SPIクロックを設定
+      cfg.pin_sclk = 18;     // SCLKが接続されているピン番号
+      cfg.pin_mosi = 23;     // MOSIが接続されているピン番号
+      cfg.pin_miso = 19;     // MISOが接続されているピン番号
+      cfg.pin_cs   =  5;     //   CSが接続されているピン番号
+
+// I2C接続の場合
+      cfg.i2c_port = 1;      // 使用するI2Cを選択 (0 or 1)
+      cfg.i2c_addr = 0x38;   // I2Cデバイスアドレス番号
+      cfg.pin_sda  = 23;     // SDAが接続されているピン番号
+      cfg.pin_scl  = 32;     // SCLが接続されているピン番号
+      cfg.freq = 400000;     // I2Cクロックを設定
+
+      _touch_instance.config(cfg);
+      _panel_instance.setTouch(&_touch_instance);  // タッチスクリーンをパネルにセットします。
+    }
+//*/
+
+    setPanel(&_panel_instance); // 使用するパネルをセットします。
+  }
+};
+
+// 準備したクラスのインスタンスを作成します。
+LGFX tft;
+/*Change to your screen resolution*/
+static const uint32_t screenWidth  = 320;
+static const uint32_t screenHeight = 240;
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[ screenWidth * 10 ];
+
+
+
+/* Display flushing */
+void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
+{
+   uint32_t w = ( area->x2 - area->x1 + 1 );
+   uint32_t h = ( area->y2 - area->y1 + 1 );
+
+   tft.startWrite();
+   tft.setAddrWindow( area->x1, area->y1, w, h );
+   //tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
+   tft.writePixels((lgfx::rgb565_t *)&color_p->full, w*h);
+   tft.endWrite();
+
+   lv_disp_flush_ready( disp );
+}
+
+/*Read the touchpad*/
+void my_touchpad_read( lv_indev_drv_t * indev_driver, lv_indev_data_t * data )
+{
+   uint16_t touchX, touchY;
+
+   bool touched = tft.getTouch( &touchX, &touchY);
+
+   if( !touched )
+   {
+      data->state = LV_INDEV_STATE_REL;
+   }
+   else
+   {
+      data->state = LV_INDEV_STATE_PR;
+
+      /*Set the coordinates*/
+      data->point.x = touchX;
+      data->point.y = touchY;
+
+      Serial.print( "Data x " );
+      Serial.println( touchX );
+
+      Serial.print( "Data y " );
+      Serial.println( touchY );
+   }
+}
+
+void setup()
+{
+   Serial.begin( 115200 ); /* prepare for possible serial debug */
+ 
+
+
+
+   tft.begin();          /* TFT init */
+   tft.setRotation( 1 ); /* Landscape orientation, flipped */
+   tft.setBrightness(255);
+   /*Set the touchscreen calibration data,
+     the actual data for your display can be aquired using
+     the Generic -> Touch_calibrate example from the TFT_eSPI library*/
+   uint16_t calData[5] = { 275, 3620, 264, 3532, 1 };
+   tft.setTouchCalibrate( calData );
+    
+   lv_init();
+   lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * 10 );
+
+   /*Initialize the display*/
+   static lv_disp_drv_t disp_drv;
+   lv_disp_drv_init( &disp_drv );
+   /*Change the following line to your display resolution*/
+   disp_drv.hor_res = screenWidth;
+   disp_drv.ver_res = screenHeight;
+   disp_drv.flush_cb = my_disp_flush;
+   disp_drv.draw_buf = &draw_buf;
+   lv_disp_drv_register( &disp_drv );
+
+   /*Initialize the (dummy) input device driver*/
+   static lv_indev_drv_t indev_drv;
+   lv_indev_drv_init( &indev_drv );
+   indev_drv.type = LV_INDEV_TYPE_POINTER;
+   indev_drv.read_cb = my_touchpad_read;
+   lv_indev_drv_register( &indev_drv );
+   //lv_example_get_started_1();
+   lv_example_get_started_1();
+   //lv_example_qrcode_1();
+
+}
+
+void loop()
+{
+   lv_timer_handler(); /* let the GUI do its work */
+   delay(5);
+}
+
+static void btn_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * btn = lv_event_get_target(e);
+    if(code == LV_EVENT_CLICKED) {
+        static uint8_t cnt = 0;
+        cnt++;
+
+        /*Get the first child of the button which is the label and change its text*/
+        lv_obj_t * label = lv_obj_get_child(btn, 0);
+        lv_label_set_text_fmt(label, "Button: %d", cnt);
+    }
+}
+
+
+/**
+ * Create a button with a label and react on click event.
+ */
+void lv_example_get_started_1(void)
+{
+    lv_obj_t * btn = lv_btn_create(lv_scr_act());     /*Add a button the current screen*/
+    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
+    lv_obj_set_size(btn, 240, 50);    /*Set its size*/
+    lv_obj_align(btn, LV_ALIGN_CENTER, 0,0);
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
+
+    lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
+    lv_label_set_text(label, "Aziz Tampan dan Seksi");                     /*Set the labels text*/
+    lv_obj_center(label);
+}
+
+/*
+void anim_x_cb(void * var, int32_t v)
+{
+    lv_obj_set_x(var, v);
+}
+
+
+void sw_event_cb(lv_event_t * e)
+{
+    lv_obj_t * sw = lv_event_get_target(e);
+    lv_obj_t * label = lv_event_get_user_data(e);
+
+    if(lv_obj_has_state(sw, LV_STATE_CHECKED)) {
+        lv_anim_t a;
+        lv_anim_init(&a);
+        lv_anim_set_var(&a, label);
+        lv_anim_set_values(&a, lv_obj_get_x(label), 100);
+        lv_anim_set_time(&a, 500);
+        lv_anim_set_exec_cb(&a, anim_x_cb);
+        lv_anim_set_path_cb(&a, lv_anim_path_overshoot);
+        lv_anim_start(&a);
+    }
+    else {
+        lv_anim_t a;
+        lv_anim_init(&a);
+        lv_anim_set_var(&a, label);
+        lv_anim_set_values(&a, lv_obj_get_x(label), -lv_obj_get_width(label));
+        lv_anim_set_time(&a, 500);
+        lv_anim_set_exec_cb(&a, anim_x_cb);
+        lv_anim_set_path_cb(&a, lv_anim_path_ease_in);
+        lv_anim_start(&a);
+    }
+
+}
+
+void lv_example_anim_1(void)
+{
+    lv_obj_t * label = lv_label_create(lv_scr_act());
+    lv_label_set_text(label, "Hello animations!");
+    lv_obj_set_pos(label, 100, 10);
+
+
+    lv_obj_t * sw = lv_switch_create(lv_scr_act());
+    lv_obj_center(sw);
+    lv_obj_add_state(sw, LV_STATE_CHECKED);
+   // lv_obj_add_event_cb(sw, sw_event_cb, LV_EVENT_VALUE_CHANGED, label);
+}*/
+void lv_example_qrcode_1(void)
+{
+    lv_color_t bg_color = lv_palette_lighten(LV_PALETTE_LIGHT_BLUE, 5);
+    lv_color_t fg_color = lv_palette_darken(LV_PALETTE_BLUE, 4);
+
+    lv_obj_t * qr = lv_qrcode_create(lv_scr_act());
+    lv_qrcode_set_size(qr, 150);
+    lv_qrcode_set_dark_color(qr, fg_color);
+    lv_qrcode_set_light_color(qr, bg_color);
+
+    /*Set data*/
+    const char * data = "https://lvgl.io";
+    lv_qrcode_update(qr, data, strlen(data));
+    lv_obj_center(qr);
+
+    /*Add a border with bg_color*/
+    lv_obj_set_style_border_color(qr, bg_color, 0);
+    lv_obj_set_style_border_width(qr, 5, 0);
+}
